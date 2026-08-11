778a5be9f7a2ccc5dc63155d7388e6e6ac1a12e2
tomwantsajobinmusic <thomas@sidewaysmgmt.com>
2026-08-11 16:02:34 -0500

    Update artists.js

--- data/artists.js
+++ data/artists.js
@@ -28,7 +28,7 @@ window.ARTISTS = {
     cardNumber: "001/010",
     rarity: "HOLO RARE",
     story: [
-      "It was pouring rain, and it was my first big festival. I remember being worried about my camera and the rented lens that I couldn’t afford to break. I was covered in mud and remember thinking to myself, “I definitely don’t look like I belong on stage plastered in mud,” but I had to get the shot. Somehow I just happened to catch Max throwing up the finger to the crowd. This was also the first photo of mine that a big artist posted; I remember sitting at my day job with my mind blown seeing it on his page. Definitely a moment I won’t ever forget.",
+      "It was pouring rain, and my first oppurtunity to shoot a big festival. I'll never forget being worried about my camera and the rented lens that I couldn’t afford to break. I was covered in mud and remember thinking to myself, “I definitely don’t look like I belong on stage plastered in mud,” but I had to get the shot. Somehow I just happened to catch Max throwing up the finger to the crowd. This was also the first photo of mine that a big artist posted; I remember sitting at my day job with my mind blown seeing it on his page. Definitely a moment I won’t ever forget.",
       "Printed as a one-of-ten holo, raised cold-stamped + UV Numoda Dragon. Not for sale, for portfolio display purposes only. If it caught your eye, please click the link below to learn more about me."
     ],
     links: [
